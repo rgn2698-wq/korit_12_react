44f144da714874d6964f44546835a9068b1d8852
rgn2698-wq <rgn2698@gmail.com>
2026-03-09 15:38:25 +0900

    20260309

---
+++ weatherapp/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+
+function App() {
+const [weather, setWeather] = useState({
+  temp : '',
+  desc : '',
+  icon : '',
+});
+
+  useEffect(() => {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=Busan&units=Metric&APIkey=API아이디`)
+  .then(res => res.json())
+  .then(data => {
+    setWeather({
+      temp: data.main.temp,
+      desc: data.weather[0].description,
+      icon: data.weather[0].icon
+    })
+  })
+  .catch(err => console.log(err));
+  }, []);
+
+  if (weather.icon) {
+      return (
+      <>
+        <p>기온 : {weather.temp}</p>
+        <p>설명 : {weather['desc']}</p>
+        <p>아이콘 string : {weather.icon}</p>
+        <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt='날씨 아이콘'/>
+      </>
+  )
+  } else {
+    return <h1>loading...⏰</h1>
+  }
+
+
+}
+
+export default App
